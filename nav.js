3e3cb3b9a811a19905bc5da7b40f6c389bfe36d9
hanshimawan26 <hanshimawan26@gmail.com>
2024-04-24 12:39:25 +0700

    color fix

--- nav.js
+++ nav.js
@@ -11,11 +11,11 @@ function scrollFunction() {
     for (let i = 0; i < navitem.length; i++) {
         navitem[i].style.color = "#064c72";
     }
-    document.getElementById("navbar").style.backgroundColor = "#f1ebe1"; /* Change the background color when scrolled */
+    document.getElementById("navbar").style.backgroundColor = "#f2dfbd"; /* Change the background color when scrolled */
   } else {
     document.getElementById("navbar").style.backgroundColor = "transparent"; /* Make transparent when at the top */
     for (let i = 0; i < navitem.length; i++) {
-        navitem[i].style.color = "#f1ebe1";
+        navitem[i].style.color = "#f2dfbd";
     }
   }
 }
